aaebcc939e2e8054feb51acef8b3ce6c327a42e7
Adityazz8 <borseaditya08@gmail.com>
2025-09-22 23:34:18 +0530

    hmm try dont know if it works

--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="SignIn" component={SignInScreen} />
